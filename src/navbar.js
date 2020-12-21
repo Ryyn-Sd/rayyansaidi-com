97767cda9404c4ff30266c422376ba91635c717c
Rayyan <63111246+Ryyn-Sd@users.noreply.github.com>
2020-12-21 17:11:37 -0500

    Fix bug again

--- src/navbar.js
+++ src/navbar.js
@@ -7,8 +7,8 @@ const Nav = (props) =>
         <li className="navbar"><b><Link href="/" className={props.act == 1 ? "active" : null}>Home</Link></b></li>
         <li><b><a>Bio</a></b></li>
         <li><a><b>Projects</b></a></li>
-        <li className="navbar"><Link to="projects/salamsaidi" className={props.act == 2 ? "active" : null}>Salamsaidi.com</Link></li>
-        <li className="navbar"><Link to="projects/suggestions" className={props.act == 3 ? "active" : null}>Suggestions</Link></li>
+        <li className="navbar"><Link to="/projects/salamsaidi" className={props.act == 2 ? "active" : null}>Salamsaidi.com</Link></li>
+        <li className="navbar"><Link to="/projects/suggestions" className={props.act == 3 ? "active" : null}>Suggestions</Link></li>
     </ul>
 
 export default Nav
