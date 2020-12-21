27444a44afc0de986836f19ab9690db19b23c19d
Rayyan <63111246+Ryyn-Sd@users.noreply.github.com>
2020-12-21 18:43:44 -0500

    Make links crawlable

--- src/navbar.js
+++ src/navbar.js
@@ -5,8 +5,8 @@ import "./style.css"
 const Nav = (props) =>
     <ul>
         <li className="navbar"><b><Link href="/" className={props.act == 1 ? "active" : null}>Home</Link></b></li>
-        <li><b><a>Bio</a></b></li>
-        <li><a><b>Projects</b></a></li>
+        <li><b><a href="#">Bio</a></b></li>
+        <li><b><a href="#">Projects</a></b></li>
         <li className="navbar"><Link to="/projects/salamsaidi" className={props.act == 2 ? "active" : null}>Salamsaidi.com</Link></li>
         <li className="navbar"><Link to="/projects/suggestions" className={props.act == 3 ? "active" : null}>Suggestions</Link></li>
     </ul>
