e27887211b5ec6ba6193c37b0b0fadac53da7895
Nathan Chu <63111210+nathanchu@users.noreply.github.com>
2021-01-24 21:43:18 -0500

    Update nathanchu.js

--- src/ads/nathanchu.js
+++ src/ads/nathanchu.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 export default () => (
-  <a href="https://nathanchu.com/?mtm_campaign=ads&mtm_kwd=rayyansaidi" style={{textDecoration:'none'}}>
-    <div style={{ width: 600, maxWidth: '100%', height: 200, backgroundColor: 'rgba(249, 250, 251)', margin: '0 auto', padding: 16, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', fontFamily: 'Montserrat, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"', color: '#000', borderRadius: '16px'}}>
+  <a href="https://nathanchu.com/?mtm_campaign=ads&mtm_kwd=rayyansaidi" style={{ textDecoration:'none' }}>
+    <div style={{ width: 600, maxWidth: '100%', height: 200, backgroundColor: 'rgba(249, 250, 251)', margin: '0 auto', padding: 16, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', fontFamily: 'Montserrat, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"', color: '#000', borderRadius: '16px' }}>
       <h1>Nathan Chu</h1>
       <p>Visit nathanchu.com to learn about some cool projects, and learn how to do some of them yourself. It's open source, too, so you can build a website just like it by looking at the source.</p>
     </div>
