5d072a6fc751ff25284f6a75733e1c8d34cd5722
Rayyan <63111246+Ryyn-Sd@users.noreply.github.com>
2021-03-03 12:22:06 -0500

    Fix error

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,16 +3,16 @@ import { Link } from 'gatsby';
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+      <h1>Page not found</h1>
+      <p>
         We couldn’t find what you were looking for.
         <br />
         {process.env.NODE_ENV === 'development' ? (
           <>
             <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
+            Try creating a page in <code>src/pages/</code>.
             <br />
           </>
         ) : null}
